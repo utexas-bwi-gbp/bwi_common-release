3110c85a16696b0318274c1ee0e3c515a9cb67c3
Jack O'Quin <jack.oquin@gmail.com>
2014-04-16 10:58:19 -0500

    Trimmed the branch to only the utexas_gdc sub directory

--- bwi_tools/src/bwi_tools/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from .mapper import loadMapFromFile, saveMapToFile
-from .roslaunch import start_roslaunch_process, stop_roslaunch_process
-from .timer import Timer
-from .wall_rate import WallRate
